8bea4e3597a8e818d9acc39f14c90ffeed838ac6
Matt Lindborg <broxthemighty@gmail.com>
2026-03-25 21:38:18 -0500

    another update to index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { registerRootComponent } from 'expo';
 import { Platform } from 'react-native';
 
-// Force the Hermes engine to attach global prototypes before continuing
+// 1. Force the Hermes engine to attach global prototypes FIRST
 if (Platform.OS === 'android') {
   require('react-native/Libraries/Core/InitializeCore');
 }
 
-// NOW we import the App logic safely
-import App from './App';
+// 2. Use require() instead of import so this DOES NOT get hoisted above InitializeCore
+const App = require('./App').default;
 
+// 3. Register the app safely
 registerRootComponent(App);
